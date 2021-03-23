03c0cdd0d29cd653c46569d1a758a76d09d55137
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-03-22 23:41:19 -0500

    Reporte de impresión de etiquetas de lotes

--- CoffeeConnect.API/Controllers/LoteController.cs
+++ CoffeeConnect.API/Controllers/LoteController.cs
@@ -1,8 +1,15 @@
-﻿using CoffeeConnect.DTO;
+﻿using AspNetCore.Reporting;
+using BoldReports.Windows;
+using BoldReports.Writer;
+using CoffeeConnect.DTO;
 using CoffeeConnect.Interface.Service;
+using Core.Common;
 using Core.Common.Domain.Model;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Integracion.Deuda.Controller
 {
@@ -12,10 +19,12 @@ namespace Integracion.Deuda.Controller
     {
         private ILoteService _loteService;
         private Core.Common.Logger.ILog _log;
-        public LoteController(ILoteService loteService, Core.Common.Logger.ILog log)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public LoteController(ILoteService loteService, Core.Common.Logger.ILog log, IWebHostEnvironment webHostEnvironment)
         {
             _loteService = loteService;
             _log = log;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet("version")]
@@ -184,7 +193,7 @@ namespace Integracion.Deuda.Controller
                 response.PromedioRendimientoPorcentaje = resultado.PromedioRendimientoPorcentaje;
                 response.PromedioHumedadPorcentaje = resultado.PromedioHumedadPorcentaje;
                 response.PromedioTotalAnalisisSensorial = resultado.PromedioTotalAnalisisSensorial;
-                
+
                 response.FechaRegistro = resultado.FechaRegistro;
                 response.UsuarioRegistro = resultado.UsuarioRegistro;
                 response.FechaUltimaActualizacion = resultado.FechaUltimaActualizacion;
@@ -207,7 +216,6 @@ namespace Integracion.Deuda.Controller
 
             return Ok(response);
         }
-
 
         [Route("ConsultarLoteDetallePorLoteId")]
         [HttpPost]
@@ -237,6 +245,75 @@ namespace Integracion.Deuda.Controller
             _log.RegistrarEvento($"{guid.ToString()}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(response)}");
 
             return Ok(response);
+        }
+
+        [Route("GenerarPDF")]
+        [HttpGet]
+        public IActionResult GenerarPDF(int id)
+        {
+            return generar(id);
+        }
+
+        private IActionResult generar(int id)
+        {
+            Guid guid = Guid.NewGuid();
+            _log.RegistrarEvento($"{guid.ToString()}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(id)}");
+
+            ConsultaNotaCompraPorGuiaRecepcionMateriaPrimaIdResponseDTO response = new ConsultaNotaCompraPorGuiaRecepcionMateriaPrimaIdResponseDTO();
+            try
+            {
+                ConsultaNotaCompraPorGuiaRecepcionMateriaPrimaIdRequestDTO request = new ConsultaNotaCompraPorGuiaRecepcionMateriaPrimaIdRequestDTO();
+                request.GuiaRecepcionMateriaPrimaId = id;
+
+                //ConsultaImpresionNotaCompraPorGuiaRecepcionMateriaPrimaIdBE item = _notaCompraService.ConsultarImpresionNotaCompraPorGuiaRecepcionMateriaPrimaId(request);
+
+                List<ConsultaImpresionNotaCompraPorGuiaRecepcionMateriaPrimaIdBE> lista = new List<ConsultaImpresionNotaCompraPorGuiaRecepcionMateriaPrimaIdBE>();
+                //lista.Add(item);
+
+                var path = $"{this._webHostEnvironment.ContentRootPath}\\Reportes\\rptEtiquetas.rdlc";
+                FileStream report = new FileStream(path, FileMode.Open, FileAccess.Read);
+                FileStream sb = new FileStream(this._webHostEnvironment.ContentRootPath + @"\Reportes\srptEtiquetaLote.rdlc", FileMode.Open, FileAccess.Read);
+                ReportWriter writer = new ReportWriter();
+                writer.ReportProcessingMode = ProcessingMode.Local;
+                writer.SubreportProcessing += LocalReport_SubreportProcessing;
+                writer.LoadSubreport("srptEtiquetaLote", sb);
+                writer.LoadReport(report);
+
+                writer.DataSources.Clear();
+                writer.DataSources.Add(new ReportDataSource { Name = "DataSet" });
+
+                MemoryStream memoryStream = new MemoryStream();
+                writer.Save(memoryStream, WriterFormat.PDF);
+
+                memoryStream.Position = 0;
+                FileStreamResult fileStreamResult = new FileStreamResult(memoryStream, "application/pdf");
+                fileStreamResult.FileDownloadName = "Invoice.pdf";
+                return fileStreamResult;
+            }
+            catch (ResultException ex)
+            {
+                response.Result = new Result() { Success = true, ErrCode = ex.Result.ErrCode, Message = ex.Result.Message };
+            }
+            catch (Exception ex)
+            {
+                response.Result = new Result() { Success = false, Message = "Ocurrio un problema en el servicio, intentelo nuevamente." };
+                _log.RegistrarEvento(ex, guid.ToString());
+            }
+
+            _log.RegistrarEvento($"{guid.ToString()}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(response)}");
+
+            return Ok(response);
+        }
+
+        private void LocalReport_SubreportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            var ID = Convert.ToInt32(e.Parameters[0].Values[0]);
+            //var employeegroup = Employees.FindAll(x => x.ID == ID);
+            //if (e.ReportPath == "EmployeeDetails")
+            //{
+            //    var employeeDetails = new ReportDataSource() { Name = "Employee_DS", Value = employeegroup };
+            //    e.DataSources.Add(employeeDetails);
+            //}
         }
     }
 }
