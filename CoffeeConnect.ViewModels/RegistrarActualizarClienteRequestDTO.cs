deebf319d2bbbe65d9384e57af80c761a2350f2f
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-04-06 23:22:27 -0500

    ajustes

--- CoffeeConnect.ViewModels/RegistrarActualizarClienteRequestDTO.cs
+++ CoffeeConnect.ViewModels/RegistrarActualizarClienteRequestDTO.cs
@@ -1,124 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoffeeConnect.DTO
+﻿namespace CoffeeConnect.DTO
 {
-	public class RegistrarActualizarClienteRequestDTO
-	{
-		#region Properties
-		public int ClienteId
-		{ get; set; }
+    public class RegistrarActualizarClienteRequestDTO
+    {
+        #region Properties
+        public int ClienteId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Numero value.
-		/// </summary>
-		public string Numero
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the Numero value.
+        /// </summary>
+        public string Numero
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the TipoClienteId value.
-		/// </summary>
-		public string TipoClienteId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the TipoClienteId value.
+        /// </summary>
+        public string TipoClienteId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Ruc value.
-		/// </summary>
-		public string Ruc
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the Ruc value.
+        /// </summary>
+        public string Ruc
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the RazonSocial value.
-		/// </summary>
-		public string RazonSocial
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the RazonSocial value.
+        /// </summary>
+        public string RazonSocial
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Direccion value.
-		/// </summary>
-		public string Direccion
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the Direccion value.
+        /// </summary>
+        public string Direccion
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the PaisId value.
-		/// </summary>
-		public int PaisId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the PaisId value.
+        /// </summary>
+        public string PaisId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the DepartamentoId value.
-		/// </summary>
-		public string DepartamentoId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the DepartamentoId value.
+        /// </summary>
+        public string DepartamentoId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the ProvinciaId value.
-		/// </summary>
-		public string ProvinciaId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the ProvinciaId value.
+        /// </summary>
+        public string ProvinciaId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the DistritoId value.
-		/// </summary>
-		public string DistritoId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the DistritoId value.
+        /// </summary>
+        public string DistritoId
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the NumeroTelefono value.
-		/// </summary>
-		public string NumeroTelefono
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the NumeroTelefono value.
+        /// </summary>
+        public string NumeroTelefono
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the CorreoElectronico value.
-		/// </summary>
-		public string CorreoElectronico
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the CorreoElectronico value.
+        /// </summary>
+        public string CorreoElectronico
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the GerenteGeneral value.
-		/// </summary>
-		public string GerenteGeneral
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the GerenteGeneral value.
+        /// </summary>
+        public string GerenteGeneral
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the GerenteGeneralNumero value.
-		/// </summary>
-		public string GerenteGeneralNumero
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the GerenteGeneralNumero value.
+        /// </summary>
+        public string GerenteGeneralNumero
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Presidente value.
-		/// </summary>
-		public string Presidente
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the Presidente value.
+        /// </summary>
+        public string Presidente
+        { get; set; }
 
-		/// <summary>
-		/// Gets or sets the PresidenteNumero value.
-		/// </summary>
-		public string PresidenteNumero
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the PresidenteNumero value.
+        /// </summary>
+        public string PresidenteNumero
+        { get; set; }
 
-		
 
-		/// <summary>
-		/// Gets or sets the UsuarioRegistro value.
-		/// </summary>
-		public string Usuario
-		{ get; set; }
 
-		
-		/// <summary>
-		/// Gets or sets the EstadoId value.
-		/// </summary>
-		public string EstadoId
-		{ get; set; }
+        /// <summary>
+        /// Gets or sets the UsuarioRegistro value.
+        /// </summary>
+        public string Usuario
+        { get; set; }
 
-		
 
-		#endregion
-	}
+        /// <summary>
+        /// Gets or sets the EstadoId value.
+        /// </summary>
+        public string EstadoId
+        { get; set; }
+
+
+
+        #endregion
+    }
 }
