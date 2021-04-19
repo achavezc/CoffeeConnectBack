dc05b75c49019b60c43c8720efc4b5cd8b17bc7f
PamelaHM <chuancahuari@outlook.com>
2021-04-18 22:37:33 -0500

    contrato

--- CoffeeConnect.API/Controllers/ContratoController.cs
+++ CoffeeConnect.API/Controllers/ContratoController.cs
@@ -1,10 +1,13 @@
 ﻿using CoffeeConnect.DTO;
+using CoffeeConnect.DTO.Adjunto;
 using CoffeeConnect.Interface.Service;
 using Core.Common.Domain.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.IO;
+using System.Net.Mime;
 
 namespace Integracion.Deuda.Controller
 {
@@ -92,7 +95,7 @@ namespace Integracion.Deuda.Controller
 
         [Route("Actualizar")]
         [HttpPost]
-        public IActionResult Actualizar([FromBody] RegistrarActualizarContratoRequestDTO request)
+        public IActionResult Actualizar(IFormFile file, [FromForm] string request)
         {
             Guid guid = Guid.NewGuid();
             _log.RegistrarEvento($"{guid.ToString()}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
@@ -100,7 +103,9 @@ namespace Integracion.Deuda.Controller
             RegistrarActualizarContratoResponseDTO response = new RegistrarActualizarContratoResponseDTO();
             try
             {
-                response.Result.Data = _contratoService.ActualizarContrato(request);
+                var myJsonObject = JsonConvert.DeserializeObject<RegistrarActualizarContratoRequestDTO>(request);
+                
+                response.Result.Data = _contratoService.ActualizarContrato(myJsonObject, file);
 
                 response.Result.Success = true;
 
@@ -148,6 +153,80 @@ namespace Integracion.Deuda.Controller
             _log.RegistrarEvento($"{guid.ToString()}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(response)}");
 
             return Ok(response);
+        }
+
+        [Route("DescargarArchivo")]
+        //[HttpPost]
+        [HttpGet()]
+        //public IActionResult DescargarArchivo([FromBody] RequestDescargarArchivoDTO request)
+        public IActionResult DescargarArchivo([FromQuery(Name = "path")] string path, [FromQuery(Name = "name")] string name)
+        {
+
+
+            DescargarArchivoRequestDTO response = new DescargarArchivoRequestDTO();
+            RequestDescargarArchivoDTO request = new RequestDescargarArchivoDTO();
+            request.PathFile = path;
+            request.ArchivoVisual = name;
+            try
+            {
+                response.Result.Data = _contratoService.DescargarArchivo(request);
+                response.Result.Success = true;
+
+                string extension = Path.GetExtension(request.PathFile);
+
+                Response.Clear();
+                switch (extension)
+                {
+                    case ".doxc":
+                        Response.Headers.Add("Content-type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+                        break;
+                    case ".jpg":
+                        Response.Headers.Add("Content-type", "image/jpeg");
+                        break;
+                    case ".png":
+                        Response.Headers.Add("Content-type", "image/png");
+                        break;
+                    case ".pdf":
+                        Response.Headers.Add("Content-type", "application/pdf");
+                        break;
+                    case ".xls":
+                        Response.Headers.Add("Content-type", "application/vnd.ms-excel");
+                        break;
+                    case ".xlsx":
+                        Response.Headers.Add("Content-type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                        break;
+                    case ".doc":
+                        Response.Headers.Add("Content-type", "application/msword");
+                        break;
+                }
+
+                //context.Response.ContentType = "Application/octet-stream";
+                //string  content = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("attachment; filename=\"{0}\"", request.ArchivoVisual)));
+
+                var contentDispositionHeader = new ContentDisposition()
+                {
+                    FileName = request.ArchivoVisual,
+                    DispositionType = "attachment"
+                };
+
+                Response.Headers.Add("Content-Length", response.Result.Data.archivoBytes.Length.ToString());
+                Response.Headers.Add("Content-Disposition", contentDispositionHeader.ToString());
+                Response.Body.WriteAsync(response.Result.Data.archivoBytes);
+
+
+
+            }
+            catch (ResultException ex)
+            {
+                response.Result = new Result() { Success = true, ErrCode = ex.Result.ErrCode, Message = ex.Result.Message };
+            }
+            catch (Exception ex)
+            {
+                response.Result = new Result() { Success = false, Message = "Ocurrio un problema en el servicio, intentelo nuevamente." };
+            }
+
+
+            return null;
         }
 
     }
