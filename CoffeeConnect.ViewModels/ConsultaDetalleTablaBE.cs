b349d30613ba826ad0eadbf49fa8f9573f97cbf0
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-06-05 23:55:07 -0500

    ajustes para inspeccion interna

--- CoffeeConnect.ViewModels/ConsultaDetalleTablaBE.cs
+++ CoffeeConnect.ViewModels/ConsultaDetalleTablaBE.cs
@@ -4,19 +4,13 @@ namespace CoffeeConnect.DTO
 {
     public class ConsultaDetalleTablaBE
     {
-
         public string Codigo { get; set; }
-
         public string CodigoTabla { get; set; }
-
         public string Label { get; set; }
-
         public string Descripcion { get; set; }
-
         public string Mnemonico { get; set; }
-
         public string Val1 { get; set; }
-
-        public string Val2 { get; set; }        
+        public string Val2 { get; set; }
+        public int IdCatalogoPadre { get; set; }
     }
 }
