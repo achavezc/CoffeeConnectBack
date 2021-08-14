a04f2af7101072ef1e57ced1fe3bc3a6668b394a
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-08-14 00:26:02 -0500

    mejoras precio rendimiento

--- CoffeeConnect.API/Controllers/PrecioDiaRendimientoController.cs
+++ CoffeeConnect.API/Controllers/PrecioDiaRendimientoController.cs
@@ -111,9 +111,6 @@ namespace CoffeeConnect.API.Controllers
             return Ok(response);
         }
 
-
-
-
         [Route("Registrar")]
         [HttpPost]
         public IActionResult Registrar(RegistrarActualizarPrecioDiaRendimientoRequestDTO request)
@@ -202,5 +199,32 @@ namespace CoffeeConnect.API.Controllers
             return Ok(response);
         }
 
+        [Route("ActualizarPrecioDiaRendimiento")]
+        [HttpPost]
+        public IActionResult Actualizar(RegistrarActualizarPrecioDiaRendimientoRequestDTO request)
+        {
+            Guid guid = Guid.NewGuid();
+            _log.RegistrarEvento($"{guid}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
+
+            RegistrarActualizarPrecioDiaRendimientoResponseDTO response = new RegistrarActualizarPrecioDiaRendimientoResponseDTO();
+            try
+            {
+                response.Result.Data = _PrecioDiaRendimientoService.ActualizarPrecioDiaRendimiento(request);
+                response.Result.Success = true;
+            }
+            catch (ResultException ex)
+            {
+                response.Result = new Result() { Success = true, ErrCode = ex.Result.ErrCode, Message = ex.Result.Message };
+            }
+            catch (Exception ex)
+            {
+                response.Result = new Result() { Success = false, Message = "Ocurrio un problema en el servicio, intentelo nuevamente." };
+                _log.RegistrarEvento(ex, guid.ToString());
+            }
+
+            _log.RegistrarEvento($"{guid}{Environment.NewLine}{Newtonsoft.Json.JsonConvert.SerializeObject(response)}");
+
+            return Ok(response);
+        }
     }
 }
