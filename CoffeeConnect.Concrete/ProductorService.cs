f33f79b68476328eaa11a2bee8d4ba5e9d66e24c
Jorge.azcarate@danovac.com <Jorge.azcarate@danovac.com>
2023-03-27 22:52:26 -0500

    hice cambio para las fechas

--- CoffeeConnect.Concrete/ProductorService.cs
+++ CoffeeConnect.Concrete/ProductorService.cs
@@ -42,13 +42,13 @@ namespace CoffeeConnect.Service
 
             var timeSpan = request.FechaFin - request.FechaInicio;
 
-            if (timeSpan.Days > 730)
+            /*if (timeSpan.Days > 730)
             {
                 throw new ResultException(new Result { ErrCode = "02", Message = "Acopio.NotaCompra.ValidacionRangoFechaMayor2anios.Label" });
-            }
+            }*/
 
             var list = _IProductorRepository.ConsultarProductor(request);
-            return list.ToList();
+            return list.ToList(); 
         }
 
         public int RegistrarProductor(RegistrarActualizarProductorRequestDTO request)
